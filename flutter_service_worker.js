cb8ded13468f2b3b9977488c61c116b2759d9347
Hermann <63232411+hermannkabi@users.noreply.github.com>
2020-12-28 13:00:20 +0200

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,9 +14,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "49e665aec27a48015612a50845972c42",
-"/": "49e665aec27a48015612a50845972c42",
-"main.dart.js": "84ad8dedd8189aebb38d626ab80991a9",
+"index.html": "db45fe1cdad09908dc2b6174363df7cc",
+"/": "db45fe1cdad09908dc2b6174363df7cc",
+"main.dart.js": "c3056f80c7b4756996f15dd116eb713d",
 "manifest.json": "ecf3b8349556927927d29a435a0cc3d5",
 "version.json": "1bde9d8cfd1b25bd391754f699545752"
 };
